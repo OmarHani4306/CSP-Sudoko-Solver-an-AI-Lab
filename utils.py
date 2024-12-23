cd8110399de75021e26c4a7214a5e38959912590
Mohamed Mohamed Ibrahim <121159820+Mohamed-Mohamed-Ibrahim@users.noreply.github.com>
2024-12-23 10:08:12 +0200

    removing constrainsts

--- utils.py
+++ utils.py
@@ -55,38 +55,10 @@ def create_sudoku_csp(puzzle):
     # for r in range(9):
     #     for c in range(9):
     #         print((r, c), domains[(r, c)])  
-    
 
-    sudoku_constraint = lambda x, y: x != y
-    constraints = {}
-    
-    # Add row constraints
-    for r in range(9):
-        for c1 in range(9):
-            for c2 in range(c1 + 1, 9):
-                constraints[((r, c1), (r, c2))] = sudoku_constraint
-    
-    # Add column constraints
-    for c in range(9):
-        for r1 in range(9):
-            for r2 in range(r1 + 1, 9):
-                constraints[((r1, c), (r2, c))] = sudoku_constraint
-    
-    # Add subgrid constraints
-    for r_b in range(0, 9, 3):
-        for c_b in range(0, 9, 3):
-            cells = [
-                (r, c)
-                for r in range(r_b, r_b + 3)
-                for c in range(c_b, c_b + 3)
-            ]
-            for i in range(len(cells)):
-                for j in range(i + 1, len(cells)):
-                    constraints[(cells[i], cells[j])] = sudoku_constraint
     
     return {
         'variables': variables,
         'domains': domains,
-        'constraints': constraints,
     }
 
